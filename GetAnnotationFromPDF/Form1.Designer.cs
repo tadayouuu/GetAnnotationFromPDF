32ad0b4d4a629e92b87576d53479de9e6450abd7
tkusu <kusu@daitecjp.com>
2026-04-08 12:05:32 +0900

    first commit

---
+++ GetAnnotationFromPDF/Form1.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace GetAnnotationFromPDF
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.execButton = new System.Windows.Forms.Button();
+			this.getFileButton = new System.Windows.Forms.Button();
+			this.targetTextBox = new System.Windows.Forms.TextBox();
+			this.getDeptButton = new System.Windows.Forms.GroupBox();
+			this.chineseCheckBox = new System.Windows.Forms.CheckBox();
+			this.label3 = new System.Windows.Forms.Label();
+			this.deptTextBox = new System.Windows.Forms.TextBox();
+			this.loadFileButton = new System.Windows.Forms.Button();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label1 = new System.Windows.Forms.Label();
+			this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+			this.radioButton1 = new System.Windows.Forms.RadioButton();
+			this.radioButton2 = new System.Windows.Forms.RadioButton();
+			this.groupBox1 = new System.Windows.Forms.GroupBox();
+			this.getDeptButton.SuspendLayout();
+			this.groupBox1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// execButton
+			// 
+			this.execButton.Font = new System.Drawing.Font("メイリオ", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+			this.execButton.Location = new System.Drawing.Point(169, 170);
+			this.execButton.Name = "execButton";
+			this.execButton.Size = new System.Drawing.Size(134, 46);
+			this.execButton.TabIndex = 0;
+			this.execButton.Text = "button1";
+			this.execButton.UseVisualStyleBackColor = true;
+			this.execButton.Click += new System.EventHandler(this.execButton_Click);
+			// 
+			// getFileButton
+			// 
+			this.getFileButton.Font = new System.Drawing.Font("メイリオ", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+			this.getFileButton.Location = new System.Drawing.Point(406, 42);
+			this.getFileButton.Name = "getFileButton";
+			this.getFileButton.Size = new System.Drawing.Size(51, 29);
+			this.getFileButton.TabIndex = 1;
+			this.getFileButton.Text = "button1";
+			this.getFileButton.UseVisualStyleBackColor = true;
+			this.getFileButton.Click += new System.EventHandler(this.getFileButton_Click);
+			// 
+			// targetTextBox
+			// 
+			this.targetTextBox.Font = new System.Drawing.Font("メイリオ", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+			this.targetTextBox.Location = new System.Drawing.Point(12, 44);
+			this.targetTextBox.Name = "targetTextBox";
+			this.targetTextBox.Size = new System.Drawing.Size(378, 25);
+			this.targetTextBox.TabIndex = 2;
+			// 
+			// getDeptButton
+			// 
+			this.getDeptButton.BackColor = System.Drawing.SystemColors.ActiveBorder;
+			this.getDeptButton.Controls.Add(this.groupBox1);
+			this.getDeptButton.Controls.Add(this.chineseCheckBox);
+			this.getDeptButton.Controls.Add(this.label3);
+			this.getDeptButton.Controls.Add(this.deptTextBox);
+			this.getDeptButton.Controls.Add(this.loadFileButton);
+			this.getDeptButton.Controls.Add(this.label2);
+			this.getDeptButton.Controls.Add(this.label1);
+			this.getDeptButton.Controls.Add(this.targetTextBox);
+			this.getDeptButton.Controls.Add(this.getFileButton);
+			this.getDeptButton.Controls.Add(this.execButton);
+			this.getDeptButton.Font = new System.Drawing.Font("メイリオ", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+			this.getDeptButton.Location = new System.Drawing.Point(0, 0);
+			this.getDeptButton.Name = "getDeptButton";
+			this.getDeptButton.Size = new System.Drawing.Size(477, 248);
+			this.getDeptButton.TabIndex = 3;
+			this.getDeptButton.TabStop = false;
+			// 
+			// chineseCheckBox
+			// 
+			this.chineseCheckBox.AutoSize = true;
+			this.chineseCheckBox.Location = new System.Drawing.Point(362, 161);
+			this.chineseCheckBox.Name = "chineseCheckBox";
+			this.chineseCheckBox.Size = new System.Drawing.Size(89, 22);
+			this.chineseCheckBox.TabIndex = 10;
+			this.chineseCheckBox.Text = "checkBox1";
+			this.chineseCheckBox.UseVisualStyleBackColor = true;
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Font = new System.Drawing.Font("メイリオ", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+			this.label3.Location = new System.Drawing.Point(14, 87);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(42, 18);
+			this.label3.TabIndex = 8;
+			this.label3.Text = "label3";
+			// 
+			// deptTextBox
+			// 
+			this.deptTextBox.Font = new System.Drawing.Font("メイリオ", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+			this.deptTextBox.Location = new System.Drawing.Point(12, 108);
+			this.deptTextBox.Name = "deptTextBox";
+			this.deptTextBox.Size = new System.Drawing.Size(378, 25);
+			this.deptTextBox.TabIndex = 7;
+			// 
+			// loadFileButton
+			// 
+			this.loadFileButton.Font = new System.Drawing.Font("メイリオ", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+			this.loadFileButton.Location = new System.Drawing.Point(406, 106);
+			this.loadFileButton.Name = "loadFileButton";
+			this.loadFileButton.Size = new System.Drawing.Size(51, 29);
+			this.loadFileButton.TabIndex = 6;
+			this.loadFileButton.Text = "button1";
+			this.loadFileButton.UseVisualStyleBackColor = true;
+			this.loadFileButton.Click += new System.EventHandler(this.loadFileButton_Click);
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+			this.label2.Location = new System.Drawing.Point(427, 203);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(44, 20);
+			this.label2.TabIndex = 4;
+			this.label2.Text = "label2";
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("メイリオ", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+			this.label1.Location = new System.Drawing.Point(14, 21);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(42, 18);
+			this.label1.TabIndex = 3;
+			this.label1.Text = "label1";
+			// 
+			// openFileDialog1
+			// 
+			this.openFileDialog1.FileName = "openFileDialog1";
+			// 
+			// radioButton1
+			// 
+			this.radioButton1.AutoSize = true;
+			this.radioButton1.Location = new System.Drawing.Point(15, 18);
+			this.radioButton1.Name = "radioButton1";
+			this.radioButton1.Size = new System.Drawing.Size(101, 22);
+			this.radioButton1.TabIndex = 11;
+			this.radioButton1.TabStop = true;
+			this.radioButton1.Text = "radioButton1";
+			this.radioButton1.UseVisualStyleBackColor = true;
+			// 
+			// radioButton2
+			// 
+			this.radioButton2.AutoSize = true;
+			this.radioButton2.Location = new System.Drawing.Point(15, 46);
+			this.radioButton2.Name = "radioButton2";
+			this.radioButton2.Size = new System.Drawing.Size(101, 22);
+			this.radioButton2.TabIndex = 12;
+			this.radioButton2.TabStop = true;
+			this.radioButton2.Text = "radioButton2";
+			this.radioButton2.UseVisualStyleBackColor = true;			
+			// 
+			// groupBox1
+			// 
+			this.groupBox1.Controls.Add(this.radioButton2);
+			this.groupBox1.Controls.Add(this.radioButton1);
+			this.groupBox1.Location = new System.Drawing.Point(12, 143);
+			this.groupBox1.Name = "groupBox1";
+			this.groupBox1.Size = new System.Drawing.Size(140, 73);
+			this.groupBox1.TabIndex = 13;
+			this.groupBox1.TabStop = false;
+			this.groupBox1.Text = "groupBox1";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(476, 230);
+			this.Controls.Add(this.getDeptButton);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.getDeptButton.ResumeLayout(false);
+			this.getDeptButton.PerformLayout();
+			this.groupBox1.ResumeLayout(false);
+			this.groupBox1.PerformLayout();
+			this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button execButton;
+        private System.Windows.Forms.Button getFileButton;
+        private System.Windows.Forms.TextBox targetTextBox;
+        private System.Windows.Forms.GroupBox getDeptButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button loadFileButton;
+        private System.Windows.Forms.TextBox deptTextBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.CheckBox chineseCheckBox;
+		private System.Windows.Forms.GroupBox groupBox1;
+		private System.Windows.Forms.RadioButton radioButton2;
+		private System.Windows.Forms.RadioButton radioButton1;
+	}
+}
+
---
+++ GetAnnotationFromPDF/Form1.cs
@@ -0,0 +1,330 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using PdfSharp.Pdf.Annotations;
+using PdfSharp.Pdf.IO;
+using ClosedXML.Excel;
+using System.Runtime.CompilerServices;
+using DocumentFormat.OpenXml.Spreadsheet;
+using Csv;
+
+namespace GetAnnotationFromPDF
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.Text = "PDFから注釈を取得（福岡原作チーム専用）";
+            this.getFileButton.Text = "...";
+            this.loadFileButton.Text = "...";
+            this.execButton.Text = "実行";
+            this.label1.Text = "PDFファイルパス:";
+            this.label2.Text = "V0.01";
+            this.label3.Text = "宛先ファイルパス:";
+            this.chineseCheckBox.Text = "中国語対応（-1）";
+            this.groupBox1.Text = "使用用途:";
+            this.radioButton1.Checked = true;
+            this.radioButton1.Text = "設計チェック用";
+            this.radioButton2.Text = "戻りまとめ用";
+			//V0.01 通常版（V0.10）から引き継ぎ、宛先をまとめないバージョンも作成
+		}
+
+        private static string GetFilePath(string filter)
+        {
+            using (var ofd = new OpenFileDialog())
+            {                
+                ofd.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+                ofd.Filter = filter;
+                ofd.FilterIndex = 2;
+                ofd.Title = "開くファイルを選択してください";
+                ofd.RestoreDirectory = true;
+                ofd.CheckFileExists = true;
+                ofd.CheckPathExists = true;
+
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    return ofd.FileName;
+                }
+                return null;
+            }
+        }
+
+		private async void execButton_Click(object sender, EventArgs e)
+		{
+			if (this.targetTextBox.Text == "")
+			{
+				MessageBox.Show("PDFファイルのあるフォルダを選択してください");
+				return;
+			}
+
+			bool checkback = this.radioButton2.Checked;
+			await Task.Run(() => LoadCommentList(this.targetTextBox.Text, this.deptTextBox.Text, this.chineseCheckBox.Checked, checkback));
+
+			MessageBox.Show("処理終了");
+			System.Diagnostics.Process.Start(Path.GetDirectoryName(this.targetTextBox.Text));
+		}
+
+		private static void LoadCommentList(string dir, string fPath, bool chinese, bool checkback) //using pdfsharp
+        {
+            var lines = File.ReadAllLines(fPath).Distinct().ToList();			
+
+			PdfSharp.Pdf.PdfDocument inputDoc = PdfSharp.Pdf.IO.PdfReader.Open(dir, PdfDocumentOpenMode.Import);
+            PdfSharp.Pdf.PdfDocument document = new PdfSharp.Pdf.PdfDocument();
+            PdfSharp.Pdf.PdfPage page = new PdfSharp.Pdf.PdfPage();
+            var CommentList = new List<Annos>();
+            var dirPath = Path.GetDirectoryName(dir);
+            var xlsxFileName = checkback ? Path.Combine(dirPath, Path.GetFileNameWithoutExtension(dir) + "_戻りまとめ用.xlsx") : Path.Combine(dirPath, Path.GetFileNameWithoutExtension(dir) + ".xlsx");
+
+            var kouki = GetKouki(Properties.Resources.kouki); //康煕部首対応
+
+			if (File.Exists(xlsxFileName))
+            {
+                File.Delete(xlsxFileName);
+            }
+
+            int page_count = 0;
+            string annotation = "";
+
+            try
+            {
+                for (int i = 0; i < inputDoc.PageCount; i++)
+                {
+                    page = inputDoc.Pages[i];
+                    page = document.AddPage(page);
+
+                    for (int p = 0; p < document.Pages[i].Annotations.Elements.Count; p++)
+                    {
+                        PdfAnnotation textAnnot = document.Pages[i].Annotations[p];
+
+                        string content = textAnnot.Contents;
+
+                        page_count = i + 1;
+                        annotation = content;
+
+                        if (content != null && content != "")
+                        {
+                            if (content.Contains("殿"))
+                            {
+                                content = content.Replace("殿\r", "\u0110"); //20201125 ','を'\u0110'に変更
+                                var dest = content.Split('\u0110');
+                                dest[1] = dest[1].Trim().Replace("\r", "\n"); //20201110 Trim追記
+                                if (dest[0].Contains('、'))
+                                {
+                                    var cls = dest[0].Split('、');
+                                    for (var n = 0; n < cls.Length; n++)
+                                    {
+                                        if(!chinese)
+                                            CommentList.Add(new Annos(cls[n].Trim(), (i + 1).ToString(), dest[1])); //20201110 Trim追記
+                                        else
+                                            CommentList.Add(new Annos(cls[n].Trim(), i.ToString(), dest[1])); //20201110 Trim追記
+                                    }
+                                }
+                                else
+                                {
+                                    if(!chinese)
+                                        CommentList.Add(new Annos(dest[0].Trim(), (i + 1).ToString(), dest[1])); //20201110 Trim追記
+                                    else
+                                        CommentList.Add(new Annos(dest[0].Trim(), i.ToString(), dest[1])); //20201110 Trim追記
+                                }
+                            }
+						}
+                    }
+                }
+            }catch(Exception ex)
+            {
+                CreateErrorLog(ex, page_count, annotation, dirPath);
+                MessageBox.Show("エラー");
+                return;
+            }
+
+            using (var wb = new XLWorkbook())
+            {
+                var ws = wb.AddWorksheet();
+                int i = 1;
+
+                ws.Cell(i, 1).Value = "No.";
+                ws.Cell(i, 2).Value = "配布先部署名";
+                ws.Cell(i, 3).Value = "設計CK該当頁";
+                ws.Cell(i, 4).Value = "SISからの連絡事項";
+                i++;
+
+                var OrderedList = CommentList.OrderBy(list => list.Content).OrderBy(list => list.ClsName);
+                foreach (var list in OrderedList)
+                {
+                    ws.Cell(i, 2).Value = list.ClsName;
+                    ws.Cell(i, 3).Value = list.Page;
+                    ws.Cell(i, 4).Value = KoukiCheck(list, kouki, dirPath) + "\n";  //康煕部首対応
+                    ws.Cell(i, 4).Style.Alignment.WrapText = true;
+                    i++;
+                }
+                ws.Columns("B:D").Style.Alignment.Vertical = XLAlignmentVerticalValues.Top;
+                ws.Columns("B:D").AdjustToContents();
+                ws.Column("D").Style.Alignment.WrapText = true;
+                ws.Column("D").Width = 50;
+                wb.SaveAs(xlsxFileName);
+            }
+            DistinctSupport(xlsxFileName, lines, checkback);
+        }
+
+		//重複確認
+		private static void DistinctSupport(string xlsxFileName, List<string> lines, bool checkback)
+		{
+			using (var wb = new XLWorkbook(xlsxFileName))
+			{
+				var ws = wb.Worksheet(1);
+				int lastRow = ws.LastRowUsed().RowNumber();
+				var new_lines = new List<string>(lines);
+				for (int i = lastRow; i > 1; i--)
+				{
+					//同じページ内に全く同じコメントがあったら1つ削除
+					if (ws.Cell(i, 2).Value == ws.Cell(i - 1, 2).Value && ws.Cell(i, 3).Value.ToString() == ws.Cell(i - 1, 3).Value.ToString() && ws.Cell(i, 4).Value == ws.Cell(i - 1, 4).Value)
+					{
+						ws.Row(i).Delete();
+					}
+					foreach (var line in lines.ToList())
+					{
+						try
+						{
+							if (ws.Cell(i, 2).Value.ToString() == line.Split('\t')[1])
+							{
+								//lines.Remove(ws.Cell(i, 1).Value.ToString());
+								ws.Cell(i, 1).Value = line.Split('\t')[0];
+								new_lines.Remove(line);
+								break;
+							}
+						}
+						catch
+						{
+							continue;
+						}
+
+					}
+				}
+
+                if (!checkback)
+                {
+					lastRow = ws.LastRowUsed().RowNumber();
+					for (int i = lastRow; i > 1; i--)
+					{
+						if (ws.Cell(i, 2).Value == ws.Cell(i - 1, 2).Value && ws.Cell(i, 4).Value == ws.Cell(i - 1, 4).Value)
+						{
+							ws.Cell(i - 1, 3).Value = ws.Cell(i - 1, 3).Value + "、" + ws.Cell(i, 3).Value;
+							ws.Row(i).Delete();
+						}
+						else
+						{
+							ws.Cell(i, 3).Value = ws.Cell(i, 3).Value + "\n"; //20221028
+							ws.Cell(i, 3).Style.NumberFormat.Format = "@";
+						}
+					}
+				}				
+
+				lastRow = ws.LastRowUsed().RowNumber();
+				int cnt = 1;
+				foreach (var line in new_lines)
+				{
+					try
+					{
+						ws.Cell(lastRow + cnt, 1).Value = line.Split('\t')[0];
+						ws.Cell(lastRow + cnt, 2).Value = line.Split('\t')[1];
+						ws.Cell(lastRow + cnt, 3).Value = "-";
+						ws.Cell(lastRow + cnt, 4).Value = "特に質問はありません。\n修正がある場合はご指示ください。\n";
+						cnt++;
+					}
+					catch
+					{
+						continue;
+					}
+
+				}
+				ws.Range(ws.Cell(2, 1), ws.Cell(lastRow + cnt, 4)).SortColumns.Add(1, XLSortOrder.Ascending, true, true);
+				ws.Range(ws.Cell(2, 1), ws.Cell(lastRow + cnt, 4)).SortColumns.Add(3, XLSortOrder.Ascending);
+				ws.Range(ws.Cell(2, 1), ws.Cell(lastRow + cnt, 4)).Sort();
+				wb.Save();
+			}
+		}
+
+		//康煕部首対応----------------------------------------------------------------------------------
+		private static Dictionary<int, int> GetKouki(string koukiStr)
+        {
+			//var csvText = File.ReadAllText(koukiPath);
+			var kouki = new Dictionary<int, int>();
+
+			// CSVファイルの各行ごとに処理をする
+			foreach (var line in CsvReader.ReadFromText(koukiStr))
+			{
+				kouki.Add(int.Parse(line["target"]), int.Parse(line["replace"]));
+			}
+            return kouki;
+		}
+        private static string KoukiCheck(Annos list, Dictionary<int, int> kouki, string dirpath)
+        {
+            using(var sw = new StreamWriter(Path.Combine(dirpath,"kouki_report_" + DateTime.Now.ToString("yyMMddhhmm") + ".tsv"), true, Encoding.UTF8))
+            {
+				string checkedStr = "";
+				foreach (var m in list.Content)
+				{
+					int code = ((int)m);
+
+					if (kouki.ContainsKey(code))
+					{
+						char new_m = (char)kouki[code];
+						checkedStr += new_m;
+                        sw.WriteLine($"{list.ClsName} {list.Page}ページ:康煕部首「{(char)code}（{code}）」を「{new_m}（{kouki[code]}）」に変更しました。");
+					}
+					else
+					{
+						checkedStr += m;
+					}
+				}
+				return checkedStr;
+			}            
+		}
+		//康煕部首対応----------------------------------------------------------------------------------
+
+		private static void CreateErrorLog(Exception ex, int page, string annotation, string dirpath)
+        {
+            var date = DateTime.Now.ToString("yyMMdd");
+            var time = DateTime.Now.ToString("HH:mm:ss");
+            using (var sw = new StreamWriter(Path.Combine(dirpath, "error_log" + date + ".tsv"), true, Encoding.UTF8))
+            {
+                sw.WriteLine($"{time}\n「{ex.Message}」\n{page}ページ\n{annotation}\n");
+            }
+        }    
+        
+        private void getFileButton_Click(object sender, EventArgs e)
+        {
+            string filter = "pdfファイル(*.pdf)|*.pdf";
+            this.targetTextBox.Text = GetFilePath(filter);
+        }
+
+        private void loadFileButton_Click(object sender, EventArgs e)
+        {
+            string filter = "txtファイル(*.txt)|*.txt";
+            this.deptTextBox.Text = GetFilePath(filter);
+        }
+
+	}
+	public class Annos
+    {
+        public string ClsName { get; set; }
+        public string Page { get; set; }
+        public string Content { get; set; }
+        
+        public Annos(string name, string page, string content)
+        {
+            this.ClsName = name;
+            this.Page = page;
+            this.Content = content;
+        }
+        
+    }
+}
